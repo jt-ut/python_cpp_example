eb30da8f8cbb314629fa825154226a63a0bf78d2
jt-ut <joshtaylor@utexas.edu>
2023-06-20 10:07:28 -0500

    Initial Commit

---
+++ src/python_cpp_example/include/math.hpp
@@ -0,0 +1,19 @@
+#ifndef PYCPPEX_MATH_HPP
+#define PYCPPEX_MATH_HPP
+
+#include <omp.h>
+
+// inline int add(int i, int j)
+// {
+//     return i + j;
+// }
+
+// inline int subtract(int i, int j)
+// {
+//     return i - j;
+// }
+
+int add(int i, int j); 
+int subtract(int i, int j);
+
+#endif
